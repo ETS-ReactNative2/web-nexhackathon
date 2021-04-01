c54db11d756a0b282685d65af2dacd5b771262f9
Pedro Augusto <p.augusto0210@gmail.com>
2021-03-31 21:39:34 -0300

    ADD: Assets | Launch landing page

---
+++ src/presenter/screens/Landing/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+function Landing() {
+  return (
+    <Container>
+      <h1>Landing</h1>
+    </Container>
+  );
+};
+
+export default Landing;

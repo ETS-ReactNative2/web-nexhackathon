371077943dda4c0cb2c3323be2783ba83a965b28
guilhermeabel99 <gpeuvis@gmail.com>
2021-04-02 22:22:31 -0300

    ADD: initial about

--- src/presenter/screens/Landing/components/About/styles.js
+++ src/presenter/screens/Landing/components/About/styles.js
@@ -9,42 +9,4 @@ export const About = styled.div`
   height: 105vh;
 `
 
-
-export const IntroductionContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: flex-end;
-`
-
-export const Date = styled.p`
-  color: var(--green--color);
-  font-size: 1rem;
-  text-align: end;
-  line-height: 1rem;
-`
-
-export const EventFormat = styled.p`
-  color: var(--green--color);
-  font-size: 1rem;
-  font-weight: 300;
-  text-align: end;
-  margin-bottom: 20px;
-`
-
-export const IntroductionTitle = styled.h3`
-  font-size: 2rem;
-  text-align: end;
-  margin-bottom: 10px;
-`
-
-export const IntroductionDescription = styled.p`
-  color: var(--white--color);
-  text-align: end;
-  width: 90%;
-  margin-bottom: 20px;
-`
-
-export const IntroductionImage = styled.img`
-  width: 20rem;
 `

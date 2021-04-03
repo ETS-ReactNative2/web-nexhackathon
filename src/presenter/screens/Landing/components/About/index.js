3f455912ee48f316e9adfc01d0d1d99c7b248970
guilhermeabel99 <gpeuvis@gmail.com>
2021-04-02 22:18:50 -0300

    teste

---
+++ src/presenter/screens/Landing/components/About/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import {
+  Wave,
+  About,
+  Title,
+  AboutDescription,
+  Instructions,
+} from '.styles'
+
+import AboutWaveImg from '../../../../../_assets/images/about_wave.svg'
+
+function AboutSection() {
+  return (
+    <About>
+      <Wave src={AboutWaveImg} />
+      <Title> COMO FUNCIONA? </Title>
+      <AboutDescription>
+        Hackathon é um evento inovador,
+        que reúne programadores,
+        designers e outros profissionais ligados
+        ao desenvolvimento de soluções em maratonas
+        de trabalho com o objetivo de criar resoluções
+        específicas para um ou vários desafios.
+       </AboutDescription>
+       <Instructions>Siga os passos a seguir para participar da competição</Instructions>
+    </About>
+  );
+};
+
+export default AboutSection

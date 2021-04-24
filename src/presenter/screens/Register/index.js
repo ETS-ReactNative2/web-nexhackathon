a72621046602f617c8a7967aea32fcea644a6ca6
guilhermeabel99 <gpeuvis@gmail.com>
2021-04-24 15:44:49 -0300

    initial setup

--- src/presenter/screens/Register/index.js
+++ src/presenter/screens/Register/index.js
@@ -1,3 +1,10 @@
 import React from 'react'
+import { useFormik } from 'formik';
+
+import * as Yup from 'yup'
+
+import LeftArrow from '../../../_assets/icons/left-arrow.svg'
+
 
 import {} './styles'
+
